48c9075c24e180c4936e7b249ee068fab279c678
Paul Edmiston <paul@edmiston.org>
2022-11-08 22:02:42 -0800

    Defined shopping cart badge behavior.

--- assets/mainScript.js
+++ assets/mainScript.js
@@ -1,5 +1,24 @@
-let shoppingTally = localStorage.getItem("shoppingTally");
+const shoppingBtn_graphic = $("#shoppingCartBtn .material-symbols-outlined");
+const shoppingBtn_badge = $("#shoppingCartBtn .badge");
+
+let shoppingTally = Number( localStorage.getItem("shoppingTally") );
+shoppingBtn_badge.text(shoppingTally);
+
+function addToCart(toAdd) {
+	shoppingBtn_badge.removeClass("setNone");
+	shoppingTally = shoppingTally + toAdd;
+	shoppingBtn_badge.text(shoppingTally);
+	localStorage.setItem("shoppingTally", shoppingTally);
+}
+
+function clearCart() {
+	shoppingBtn_graphic.addClass("unfilled");
+	shoppingBtn_badge.addClass("setNone");
+	shoppingTally = 0;
+	localStorage.removeItem("shoppingTally");
+}
+
 
 $("#shoppingCartBtn").click(() => {
-	$("#shoppingCartBtn .material-symbols-outlined").toggleClass("unfilled");
+	shoppingBtn_graphic.toggleClass("unfilled");
 });
