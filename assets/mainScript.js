1e6a756590c16cb7c5a56a3a5a62835ed6b87360
Paul Edmiston <paul@edmiston.org>
2022-11-09 13:29:46 -0800

    Aside syncing completed. Beginning to populate rest of homepage.

--- assets/mainScript.js
+++ assets/mainScript.js
@@ -1,5 +1,6 @@
 twemoji.parse(document.body);
 
+
 const 
 	headerTop = $("#headerTop"),
 	nav = $("nav"),
@@ -13,21 +14,56 @@ const
 
 	pageDarken = $("#pageDarken");
 
+
 /* Set up shop. Adding these transitions now rather than in the CSS allows for them to *not* be seen when we first load the page. Some of these styles also rely on dynamic variables that CSS cannot handle.
 Is there a more graceful way to do this directly through the CSS? Probably. */
 nav.css("transition", "transform var(--uniTrans_time_NoDel)");
+aside.css("transition", "transform var(--uniTrans_time_NoDel)");
 pageDarken.css("transition", "var(--uniTrans)");
 
 nav.css( "top", headerTop.outerHeight() );
 main.css("margin-top", headerTop.outerHeight() );
 
+
+
+
+/* We want the Hamburger and Prototype disclaimer to operate in sync. By checking if both are disabled at *both* the start and end of the switch-arounds, we can ensure this.
+I know, there's probably a more graceful way to do this. */
+function toggleDarken() {
+	if ( nav.hasClass("foldIn_top") && aside.hasClass("foldIn_bottom") ) {
+		pageDarken.toggleClass("disabled");
+	}
+}
+
 hamburgerMenu.on("click", () => {
+	toggleDarken();
+
+	aside.addClass("foldIn_bottom");
+	aside.removeClass("overlaid");
+
+	nav.addClass("overlaid");
 	nav.toggleClass("foldIn_top");
-	pageDarken.toggleClass("disabled");
+
+	toggleDarken();
 } );
+
+function toggleAside() {
+	toggleDarken();
+
+	nav.addClass("foldIn_top");
+	nav.removeClass("overlaid");
+
+	aside.addClass("overlaid");
+	aside.toggleClass("foldIn_bottom");
+
+	toggleDarken();
+}
+asideTrigger.on("click", toggleAside );
+asideClose.on("click", toggleAside );
 
 pageDarken.on("click", () => {
 	nav.addClass("foldIn_top");
+	aside.addClass("foldIn_bottom");
 	pageDarken.addClass("disabled");
 } );
 
@@ -43,6 +79,8 @@ $(".scrollDown").on("click", (event) => {
 		scrollTop: scrollEnd
 		},'slow', 'swing');
 } );
+
+
 
 
 const 
